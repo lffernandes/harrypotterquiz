bcba5309b3163b5e8818638a230488b3352d165b
22937882889 <lfmonteiro@apoioprodesp.sp.gov.br>
2021-01-31 22:55:48 -0300

    create quiz

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-
-
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
     width: 100%;
@@ -20,29 +21,56 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name);
   return (
-      <QuizBackground backgroundImage={db.bg}>
-          <QuizContainer>
-           <Widget>
-               <Widget.Header>
-                <h1> The Harry Potter</h1>
-               </Widget.Header>
-               <Widget.Content>
-                <p>lorem ipsum dolor sit amet...</p>
-               </Widget.Content>
-           </Widget>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>
+          HarryPotterQuiz - Imersão React - Alura
+        </title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1> The Harry Potter</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
 
-           <Widget>
-                <Widget.Content>
-                 <h1> Quizes da Galera</h1>
-                </Widget.Content>
-               <Widget.Content>
-                <p>lorem ipsum dolor sit amet...</p>
-               </Widget.Content>
-           </Widget>
-           <footer />
-          </QuizContainer>
-          <GitHubCorner projectUrl="https://github.com/lffernandes" />
-      </QuizBackground>
-  )
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Digite seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1> Quizes da Galera</h1>
+          </Widget.Content>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/lffernandes" />
+    </QuizBackground>
+  );
 }
